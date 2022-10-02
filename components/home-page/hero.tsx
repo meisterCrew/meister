0faaf37421cb513cc9c6b24d61fe5edac40e30ca
meisterCrew <silsunggay001@gmail.com>
2022-10-02 20:48:04 +0900

    adf

---
+++ components/home-page/hero.tsx
@@ -0,0 +1,7 @@
+import { Avatar } from "@mantine/core";
+
+function Hero() {
+  return <Avatar src="images/meister.jpg" alt="it's me" radius="xl" size={80}/>;
+}
+
+export default Hero;
--- components/layout/main-navigation.tsx
+++ components/layout/main-navigation.tsx
@@ -1,18 +1,55 @@
+import { Burger, Menu, Text } from "@mantine/core";
+import {
+  IconSettings,
+  IconMessageCircle,
+  IconPhoto,
+  IconSearch,
+  IconArrowsLeftRight,
+  IconTrash,
+  IconArticle,
+  IconAccessible,
+  IconFriends,
+} from "@tabler/icons";
 import Link from "next/link";
+import { useState } from "react";
+import Hero from "../home-page/hero";
 
 import Logo from "./logo";
 import classes from "./main-navigation.module.css";
 
 export default function MainNavigation() {
+  const [opened, setOpened] = useState(false);
+  const title = opened ? "Close navigation" : "Open navigation";
   return (
     <header className={classes.header}>
+      <Hero />
       <Link href="/">
         <a>
           <Logo />
         </a>
       </Link>
-      <nav >
-        <ul >
+      <Menu shadow="md" width={200}>
+        <Menu.Target>
+          <Burger
+            color="white"
+            opened={opened}
+            onClick={() => setOpened((o) => !o)}
+            title={title}
+          />
+        </Menu.Target>
+
+        <Menu.Dropdown>
+          <Menu.Label>메뉴</Menu.Label>
+          <Link href="/posts">
+            <Menu.Item icon={<IconArticle size={14} />}>게시글</Menu.Item>
+          </Link>
+          <Link href="/members">
+            <Menu.Item icon={<IconFriends size={14} />}>부원 소개</Menu.Item>
+          </Link>
+        </Menu.Dropdown>
+      </Menu>
+      {/* <nav>
+        <ul>
           <li>
             <Link href="/posts">게시글</Link>
           </li>
@@ -23,7 +60,7 @@ export default function MainNavigation() {
             <Link href="/members">부원 소개</Link>
           </li>
         </ul>
-      </nav>
+      </nav> */}
     </header>
   );
 }
